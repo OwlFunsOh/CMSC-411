423eb4201c7e6ca5a0ea93e3288e5adf90eb7763
alfonso martinez <144267210+OwlFunsOh@users.noreply.github.com>
2023-12-08 12:30:26 -0500

    Organized + addu + subu

---
+++ functions.py
@@ -0,0 +1,149 @@
+
+
+#get memory will populate the IMemory.txt into iMemoryList
+def getMemory(filename):
+    memoryFile = open(filename, "r")
+    for line in memoryFile:
+        iMemoryList.append(line.strip())
+    memoryFile.close()
+    
+#fetch instruction will get all the instructions
+#and put them in a list.
+def fetchInstruction(filename):
+    instructionFile = open(filename, "r")
+    
+    #instruction byte will be a list containing
+    #each byte of instructions. 1 byte of instruction
+    #is one line of the text file
+    instructionByte = []
+    for line in instructionFile:
+        instructionByte.append(line.strip())
+    instructionFile.close()
+
+    
+    combineBytes(instructionByte)
+    
+#This function will combine 4 bytes together to make
+#a full 32 bit instruction
+def combineBytes(list):
+    fullInstruction = ""
+    counter = 0
+    for byte in list:
+        if (counter % 4 == 0) and (counter != 0):
+            instructionList.append(fullInstruction)
+            fullInstruction = byte
+        else:
+            fullInstruction = fullInstruction + byte
+        counter += 1
+
+#This function will take the list of instructions and separate the bits
+#into it's designated bit sizes. The control will determine what type of
+#instruction the current instruction is
+def instructionMemory(instructionList):
+    instruction = instructionList[instructionCounter]
+    return instruction
+
+def binary_to_decimal(binary_string):
+  decimal_value = 0
+  power = 0
+
+  for digit in reversed(binary_string):
+    decimal_value += int(digit) * 2**power
+    power += 1
+
+  return decimal_value
+
+def decimal_to_binary(decimal_number):
+
+  binary_string = ""
+
+  while decimal_number > 0:
+    remainder = decimal_number % 2
+    binary_string = str(remainder) + binary_string
+    decimal_number //= 2
+
+  binary_string = binary_string.zfill(32)
+  return binary_string
+
+def populateRegisters():
+    registers = []
+    zero = "00000000000000000000000000000000"
+    for i in range(32):
+        registers.append(zero)
+    return registers
+
+
+#increments the program counter by 1
+def programCounter(counter):
+    counter += 1
+    
+#control unit will take the first 6 bits of the instruction as an input
+#and determine what instruction should be ran
+def controlUnit(instruction):
+    opcode = instruction[0:6]
+    if(opcode == "000000"):
+        return aluControl(instruction[26:])
+    elif(opcode == "001001"):
+        return "addiu"
+    elif(opcode == "000100"):
+        return "beq"
+    elif(opcode == "000010"):
+        return "j"
+    elif(opcode == "100011"):
+        return "lw"
+    elif(opcode == "101011"):
+        return "sw"
+    else:
+        return "halt"
+    
+#this function will only be ran if it is an R type instruction
+#It will get the func code of the instruction as the input and output
+#the operation the ALU has to do
+def aluControl(funcCode):
+    if(funcCode == "100001"):
+        return "addu"
+    elif(funcCode == "100011"):
+        return "subu"
+    elif(funcCode == "100100"):
+        return "and"
+    elif(funcCode == "100101"):
+        return "or"
+
+def binaryAdder(num1, num2):
+    #Converting to base 2 format
+    intNum1 = int(num1, 2)
+    intNum2 = int(num2, 2)
+    
+    sum = intNum1 + intNum2
+    
+    #Converting back to binary
+    finalString = bin(sum)[2:]
+    return finalString
+
+def addu(instruction):
+    targetReg = binary_to_decimal(instruction[11:16])
+    sourceReg = binary_to_decimal(instruction[6:11])
+    destinationReg = binary_to_decimal(instruction[16:21])
+    
+    num1 = binary_to_decimal(REGISTERS[targetReg])
+    num2 = binary_to_decimal(REGISTERS[sourceReg])
+    sum = num1 + num2
+    REGISTERS[destinationReg] = decimal_to_binary(sum)
+    
+def subu(instruction):
+    targetReg = binary_to_decimal(instruction[11:16])
+    sourceReg = binary_to_decimal(instruction[6:11])
+    destinationReg = binary_to_decimal(instruction[16:21])
+    
+    num1 = binary_to_decimal(REGISTERS[targetReg])
+    num2 = binary_to_decimal(REGISTERS[sourceReg])
+    difference = num1 - num2
+    REGISTERS[destinationReg] = decimal_to_binary(difference)
+    
+    
+iMemoryList = []
+instructionList = []
+instructionCounter = 0
+currentInstruction = ""
+REGISTERS = populateRegisters()
+    
--- main.py
+++ main.py
@@ -2,123 +2,28 @@
 This program will be doing the CMSC 411 project of
 creating a single cycle processor using MIPS architecture
 """
+import functions
 
 INSTRUCTION_FILENAME = "Instructions1.txt"
 MEMORY_FILENAME = "Memory1.txt"
 INITIAL_MEMORY = "IMemory.txt"
-
-
-def populateRegisters():
-    registers = []
-    zero = "00000000000000000000000000000000"
-    for i in range(32):
-        registers.append(zero)
-    return registers
-
-#get memory will populate the IMemory.txt into iMemoryList
-def getMemory(filename):
-    memoryFile = open(filename, "r")
-    for line in memoryFile:
-        iMemoryList.append(line.strip())
-    memoryFile.close()
-
-#fetch instruction will get all the instructions
-#and put them in a list.
-def fetchInstruction(filename):
-    instructionFile = open(filename, "r")
-    
-    #instruction byte will be a list containing
-    #each byte of instructions. 1 byte of instruction
-    #is one line of the text file
-    instructionByte = []
-    for line in instructionFile:
-        instructionByte.append(line.strip())
-    instructionFile.close()
-
-    
-    combineBytes(instructionByte)
-
-#This function will combine 4 bytes together to make
-#a full 32 bit instruction
-def combineBytes(list):
-    fullInstruction = ""
-    counter = 0
-    for byte in list:
-        if (counter % 4 == 0) and (counter != 0):
-            instructionList.append(fullInstruction)
-            fullInstruction = byte
-        else:
-            fullInstruction = fullInstruction + byte
-        counter += 1
-
-#increments the program counter by 1
-def programCounter(counter):
-    counter += 1
-
-#This function will take the list of instructions and separate the bits
-#into it's designated bit sizes. The control will determine what type of
-#instruction the current instruction is
-def instructionMemory(instructionList):
-    instruction = instructionList[instructionCounter]
-    return instruction
-    
-#control unit will take the first 6 bits of the instruction as an input
-#and determine what instruction should be ran
-def controlUnit(instruction):
-    opcode = instruction[0:6]
-    if(opcode == "000000"):
-        return aluControl(instruction[26:])
-    elif(opcode == "001001"):
-        return "addiu"
-    elif(opcode == "000100"):
-        return "beq"
-    elif(opcode == "000010"):
-        return "j"
-    elif(opcode == "100011"):
-        return "lw"
-    elif(opcode == "101011"):
-        return "sw"
-    else:
-        return "halt"
-    
-#this function will only be ran if it is an R type instruction
-#It will get the func code of the instruction as the input and output
-#the operation the ALU has to do
-def aluControl(funcCode):
-    if(funcCode == "100001"):
-        return "addu"
-    elif(funcCode == "100011"):
-        return "subu"
-    elif(funcCode == "100100"):
-        return "and"
-    elif(funcCode == "100101"):
-        return "or"
-
-def binaryAdder(num1, num2):
-    #Converting to base 2 format
-    intNum1 = int(num1, 2)
-    intNum2 = int(num2, 2)
-    
-    sum = intNum1 + intNum2
-    
-    #Converting back to binary
-    finalString = bin(sum)[2:]
-    return finalString
-
-def addu(instruction):
-    pass
-
-
+"""
 iMemoryList = []
 instructionList = []
 instructionCounter = 0
 currentInstruction = ""
-REGISTERS = populateRegisters()
+REGISTERS = functions.populateRegisters()
 
+"""
 
 if __name__ == "__main__":
-    fetchInstruction(INSTRUCTION_FILENAME)
-    myInstruction = "00000000000000000000000000100101"
-    print(controlUnit(myInstruction))
+    functions.fetchInstruction(INSTRUCTION_FILENAME)
+    functions.getMemory(INITIAL_MEMORY)
+    functions.REGISTERS[0] = "00000000000000000000000000001100"
+    functions.REGISTERS[1] = "00000000000000000000000000001100"
+    functions.subu("00000000000000010000100000000000")
+    print(functions.REGISTERS)
+
+    
     
     
